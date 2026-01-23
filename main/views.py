11d208ca5396ad092db4f3e3384aa120a52615ee
kleyman-228 <llejmon@gmail.com>
2026-01-23 21:31:00 +0300

    Fix Dot

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import json
 from django.http import JsonResponse
-from LHC_Simulator import SimulationEvent, load_particles
+from .LHC_Simulator import SimulationEvent, load_particles
 
 
 Load_particle = False
