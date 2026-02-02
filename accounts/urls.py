e8461e415f6d68ec4d7d5452e98b456268970553
kleyman-228 <llejmon@gmail.com>
2026-02-02 16:17:10 +0300

    USA illegal Migrations

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import signup_view, login_view
 
 urlpatterns = [
-    path('api/signup/', signup_view, name='signup'),
-    path('api/login/', login_view, name='login'),
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
     # ...
 ]
