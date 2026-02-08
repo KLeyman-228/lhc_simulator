3246ce10d4f5c49be61aac0100a228346ebb8f0a
kleyman-228 <llejmon@gmail.com>
2026-02-08 14:52:05 +0300

    HotFix Auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,22 +1,22 @@
 from django.urls import path
-import views
+from views import signup_view, logout_view, login_view, get_my_stats, get_leaderboard
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Профиль
-    path('profile/', views.get_profile, name='profile'),  # Профиль с рангом
-    path('profile/update/', views.update_profile, name='update_profile'),
+    #path('profile/', get_profile, name='profile'),  # Профиль с рангом
+    #path('profile/update/', update_profile, name='update_profile'),
     
     # Симуляции
-    path('simulations/', views.get_my_simulations, name='my_simulations'),  # История симуляций
-    path('simulations/run/', views.run_simulation, name='run_simulation'),
+    #path('simulations/', get_my_simulations, name='my_simulations'),  # История симуляций
+    #path('simulations/run/', run_simulation, name='run_simulation'),
     
     # Статистика и рейтинг
-    path('stats/', views.get_my_stats, name='my_stats'),  # Полная статистика
-    path('leaderboard/', views.get_leaderboard, name='leaderboard'),  # Таблица лидеров
+    path('stats/', get_my_stats, name='my_stats'),  # Полная статистика
+    path('leaderboard/', get_leaderboard, name='leaderboard'),  # Таблица лидеров
 ]
